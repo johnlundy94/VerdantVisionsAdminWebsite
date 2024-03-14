5faf7500e5c5da62ece3a75142fd9738df78f8a7
John Lundy <johnclundyy@gmail.com>
2024-03-13 19:36:02 -0600

    Added routes to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import QuoteManage from "./pages/QuoteManage/QuoteManage";
+import Communication from "./pages/Communication/Communication";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/quoteManage" element={<QuoteManage />} />
+          <Route path="/communication" element={<Communication />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
