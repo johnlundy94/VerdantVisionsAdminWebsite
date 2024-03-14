1b69182a3c4a9974405a933fa202eb12ce3c752d
John Lundy <johnclundyy@gmail.com>
2024-03-14 12:43:30 -0600

    Updated dashboard to represent data

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,19 @@
 import "./Dashboard.css";
 
 function Dashboard() {
-  return <div className="dashboard-container">Dashboard</div>;
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <h2>Recent Activities</h2>
+      <h2>New Quote Requests</h2>
+      <h2>Completed Projects</h2>
+      <h2>Customer Inquiries</h2>
+      <h2>Pending Actions</h2>
+      <h2>Quotes Awaiting Approval</h2>
+      <h2>Unanswered Customer Emails</h2>
+      <h2>Quick Stats</h2>
+    </div>
+  );
 }
 
 export default Dashboard;
