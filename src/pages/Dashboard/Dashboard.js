92e8cc23406ae2463d2319b58e3799ca75c26fbf
John Lundy <johnclundyy@gmail.com>
2024-03-14 13:45:24 -0600

    Added mui drawer

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,19 +1,77 @@
 import "./Dashboard.css";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+// ... import other icons
+
+const drawerWidth = 240;
 
 function Dashboard() {
+  const pages = [
+    "Home",
+    "Quotes",
+    "Completed Projects",
+    "Customer Communication",
+    "Quick Stats",
+  ];
+
   return (
-    <div className="dashboard-container">
-      <h1>Dashboard</h1>
-      <h2>Recent Activities</h2>
-      <h2>New Quote Requests</h2>
-      <h2>Completed Projects</h2>
-      <h2>Customer Inquiries</h2>
-      <h2>Pending Actions</h2>
-      <h2>Quotes Awaiting Approval</h2>
-      <h2>Unanswered Customer Emails</h2>
-      <h2>Quick Stats</h2>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            LOGO
+          </Typography>
+        </Toolbar>
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {pages.map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {/* Replace with appropriate icons */}
+                  {index % 2 === 0 ? <MailIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* Content here */}
+      </Box>
+    </Box>
   );
 }
 
 export default Dashboard;
+
+{
+  /* <h1>Dashboard</h1>
+<h2>Recent Activities</h2>
+<h2>New Quote Requests</h2>
+<h2>Completed Projects</h2>
+<h2>Customer Inquiries</h2>
+<h2>Pending Actions</h2>
+<h2>Quotes Awaiting Approval</h2>
+<h2>Unanswered Customer Emails</h2>
+<h2>Quick Stats</h2> */
+}
