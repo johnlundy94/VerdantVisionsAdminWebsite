7207ae5b5beee204dffb0de545b5f89e4ebd177f
John Lundy <johnclundyy@gmail.com>
2024-04-01 19:03:27 -0600

    Setting up redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import quotesReducer from "./quotesSlice";
 import customersReducer from "./customersSlice";
 import alertsReducer from "./alertsSlice";
 
-export const store = configureStore({
-  reducer: {
-    quotes: quotesReducer,
-    customers: customersReducer,
-    alerts: alertsReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  quotes: quotesReducer,
+  customers: customersReducer,
+  alerts: alertsReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
